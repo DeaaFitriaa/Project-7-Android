5fa97c7a343c3b8079797ced569a85939765acaa
Dea Fitria <deyafitriaa@gmail.com>
2022-12-18 20:40:42 +0700

    First Commit

---
+++ app/src/main/java/g10/manga/loveappetite/MainActivity.java
@@ -0,0 +1,15 @@
+package g10.manga.loveappetite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceSt
+                ate);
+        setContentView(R.layout.activity_main);
+    }
+}
