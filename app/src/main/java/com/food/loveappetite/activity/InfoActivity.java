39c99168210bc418baa3ed35bb96f3d481e5b797
Dea Fitria <deyafitriaa@gmail.com>
2022-12-26 15:49:28 +0700

    Progress BasketFragment, TransactionsAdapter

--- app/src/main/java/com/food/loveappetite/activity/InfoActivity.java
+++ app/src/main/java/com/food/loveappetite/activity/InfoActivity.java
@@ -1,16 +1,86 @@
 package com.food.loveappetite.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.food.loveappetite.R;
+import com.food.loveappetite.controller.TransactionsController;
+import com.food.loveappetite.model.ProductsModel;
+import com.food.loveappetite.model.TransactionsModel;
+import com.food.loveappetite.model.UsersModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class InfoActivity extends AppCompatActivity {
+
+    private TransactionsController controller;
+    private TransactionsModel transactionsModel;
+    private ProductsModel productModel;
+    private UsersModel userModel;
+
+    private ImageView ivProduct;
+    private TextView tvName;
+    private TextView tvDesc;
+    private TextView tvPrice;
+    private Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        if (productModel == null)
+            productModel = (ProductsModel) getIntent().getSerializableExtra("Product");
+
+        if (userModel == null)
+            userModel = MainActivity.getUsersModel();
+
+        if (controller == null)
+            controller = new TransactionsController(userModel);
+
+        ivProduct = findViewById(R.id.iv_product);
+        tvName = findViewById(R.id.tv_product_name);
+        tvDesc = findViewById(R.id.tv_product_desc);
+        tvPrice = findViewById(R.id.tv_product_price);
+
+        Glide.with(this)
+                .load(productModel.getImageURL())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(ivProduct);
+        tvName.setText(productModel.getName());
+        tvDesc.setText(productModel.getDescription());
+        tvPrice.setText(productModel.getPrice());
+
+        btnAdd = findViewById(R.id.btn_add);
+        btnAdd.setOnClickListener(view -> {
+            transactionsModel = new TransactionsModel();
+            transactionsModel.setUser(userModel);
+            transactionsModel.setProduct(productModel);
+            controller.create(transactionsModel, new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    if (task.isSuccessful())
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Pesanan Telah Dibuat",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    else
+                        Toast.makeText(
+                                getApplicationContext(),
+                                "Gagal Membuat Pesanan",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                }
+            });
+        });
     }
 }
--- app/src/main/java/com/food/loveappetite/fragment/BasketFragment.java
+++ app/src/main/java/com/food/loveappetite/fragment/BasketFragment.java
@@ -2,59 +2,44 @@ package com.food.loveappetite.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.food.loveappetite.R;
+import com.food.loveappetite.activity.MainActivity;
+import com.food.loveappetite.controller.ProductsController;
+import com.food.loveappetite.controller.TransactionsController;
+import com.food.loveappetite.model.TransactionsModel;
+import com.food.loveappetite.model.UsersModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BasketFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
 public class BasketFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TransactionsController controller;
+    private List<TransactionsModel> transactionsList;
+    private UsersModel userModel;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private TextView tvUserName;
+    private RecyclerView rvBasket;
+
+    private int cvBasketId;
+    private int tvProductNameId;
+    private int tvProductPriceId;
 
     public BasketFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BasketFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BasketFragment newInstance(String param1, String param2) {
-        BasketFragment fragment = new BasketFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +47,36 @@ public class BasketFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_basket, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        tvUserName = view.findViewById(R.id.tv_user_name);
+        rvBasket = view.findViewById(R.id.rv_basket);
+
+        cvBasketId = R.id.cv_basket;
+        tvProductNameId = R.id.tv_product_name;
+        tvProductPriceId = R.id.tv_product_price;
+
+        rvBasket.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        userModel = MainActivity.getUsersModel();
+        if (controller == null)
+            controller = new TransactionsController(userModel);
+
+        controller.readAll(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DataSnapshot dataSnapshot : task.getResult().getChildren()) {
+                        TransactionsModel transaction = dataSnapshot.getValue(TransactionsModel.class);
+                        transactionsList.add(transaction);
+                    }
+
+                }
+            }
+        });
     }
 }
